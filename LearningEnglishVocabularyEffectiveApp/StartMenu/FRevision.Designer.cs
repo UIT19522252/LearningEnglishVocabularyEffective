f83b6193e878f797708662033b20956211932575
UIT19522006 <67238266+UIT19522006@users.noreply.github.com>
2020-10-14 18:44:14 +0700

    Giao diện Revision

---
+++ LearningEnglishVocabularyEffectiveApp/StartMenu/FRevision.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace StartMenu
+{
+	partial class FRevision
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FRevision));
+			this.FRevisions = new Guna.UI2.WinForms.Guna2BorderlessForm(this.components);
+			this.CtrlClose = new Guna.UI2.WinForms.Guna2ControlBox();
+			this.CtrlMini = new Guna.UI2.WinForms.Guna2ControlBox();
+			this.btnBack = new Guna.UI2.WinForms.Guna2Button();
+			this.btnRandom10 = new Guna.UI2.WinForms.Guna2Button();
+			this.guna2TextBox1 = new Guna.UI2.WinForms.Guna2TextBox();
+			this.btnAllCard = new Guna.UI2.WinForms.Guna2Button();
+			this.guna2TextBox2 = new Guna.UI2.WinForms.Guna2TextBox();
+			this.SuspendLayout();
+			// 
+			// FRevisions
+			// 
+			this.FRevisions.ContainerControl = this;
+			// 
+			// CtrlClose
+			// 
+			this.CtrlClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.CtrlClose.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(152)))), ((int)(((byte)(166)))));
+			this.CtrlClose.HoverState.Parent = this.CtrlClose;
+			this.CtrlClose.IconColor = System.Drawing.Color.White;
+			this.CtrlClose.Location = new System.Drawing.Point(1166, 2);
+			this.CtrlClose.Name = "CtrlClose";
+			this.CtrlClose.ShadowDecoration.Parent = this.CtrlClose;
+			this.CtrlClose.Size = new System.Drawing.Size(30, 24);
+			this.CtrlClose.TabIndex = 0;
+			// 
+			// CtrlMini
+			// 
+			this.CtrlMini.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.CtrlMini.ControlBoxType = Guna.UI2.WinForms.Enums.ControlBoxType.MinimizeBox;
+			this.CtrlMini.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(152)))), ((int)(((byte)(166)))));
+			this.CtrlMini.HoverState.Parent = this.CtrlMini;
+			this.CtrlMini.IconColor = System.Drawing.Color.White;
+			this.CtrlMini.Location = new System.Drawing.Point(1130, 2);
+			this.CtrlMini.Name = "CtrlMini";
+			this.CtrlMini.ShadowDecoration.Parent = this.CtrlMini;
+			this.CtrlMini.Size = new System.Drawing.Size(30, 24);
+			this.CtrlMini.TabIndex = 0;
+			// 
+			// btnBack
+			// 
+			this.btnBack.BackColor = System.Drawing.Color.Transparent;
+			this.btnBack.BorderColor = System.Drawing.Color.Transparent;
+			this.btnBack.CheckedState.Parent = this.btnBack;
+			this.btnBack.CustomImages.Parent = this.btnBack;
+			this.btnBack.FillColor = System.Drawing.Color.Transparent;
+			this.btnBack.Font = new System.Drawing.Font("Segoe UI", 9F);
+			this.btnBack.ForeColor = System.Drawing.Color.Transparent;
+			this.btnBack.HoverState.Parent = this.btnBack;
+			this.btnBack.Image = ((System.Drawing.Image)(resources.GetObject("btnBack.Image")));
+			this.btnBack.ImageOffset = new System.Drawing.Point(20, 0);
+			this.btnBack.ImageSize = new System.Drawing.Size(100, 100);
+			this.btnBack.Location = new System.Drawing.Point(52, 41);
+			this.btnBack.Name = "btnBack";
+			this.btnBack.ShadowDecoration.Parent = this.btnBack;
+			this.btnBack.Size = new System.Drawing.Size(122, 88);
+			this.btnBack.TabIndex = 2;
+			this.btnBack.Text = "guna2Button3";
+			// 
+			// btnRandom10
+			// 
+			this.btnRandom10.AutoRoundedCorners = true;
+			this.btnRandom10.BackColor = System.Drawing.Color.Transparent;
+			this.btnRandom10.BorderRadius = 60;
+			this.btnRandom10.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton;
+			this.btnRandom10.CheckedState.Parent = this.btnRandom10;
+			this.btnRandom10.CustomImages.Parent = this.btnRandom10;
+			this.btnRandom10.Font = new System.Drawing.Font("Segoe UI", 9F);
+			this.btnRandom10.ForeColor = System.Drawing.Color.White;
+			this.btnRandom10.HoverState.Parent = this.btnRandom10;
+			this.btnRandom10.Image = ((System.Drawing.Image)(resources.GetObject("btnRandom10.Image")));
+			this.btnRandom10.ImageSize = new System.Drawing.Size(500, 420);
+			this.btnRandom10.Location = new System.Drawing.Point(63, 245);
+			this.btnRandom10.Name = "btnRandom10";
+			this.btnRandom10.ShadowDecoration.Parent = this.btnRandom10;
+			this.btnRandom10.Size = new System.Drawing.Size(474, 406);
+			this.btnRandom10.TabIndex = 1;
+			// 
+			// guna2TextBox1
+			// 
+			this.guna2TextBox1.BackColor = System.Drawing.Color.Transparent;
+			this.guna2TextBox1.BorderColor = System.Drawing.Color.Transparent;
+			this.guna2TextBox1.BorderThickness = 0;
+			this.guna2TextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+			this.guna2TextBox1.DefaultText = "Sâsas";
+			this.guna2TextBox1.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+			this.guna2TextBox1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+			this.guna2TextBox1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+			this.guna2TextBox1.DisabledState.Parent = this.guna2TextBox1;
+			this.guna2TextBox1.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+			this.guna2TextBox1.FillColor = System.Drawing.Color.Transparent;
+			this.guna2TextBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+			this.guna2TextBox1.FocusedState.Parent = this.guna2TextBox1;
+			this.guna2TextBox1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+			this.guna2TextBox1.ForeColor = System.Drawing.Color.Transparent;
+			this.guna2TextBox1.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+			this.guna2TextBox1.HoverState.Parent = this.guna2TextBox1;
+			this.guna2TextBox1.Location = new System.Drawing.Point(288, 1178);
+			this.guna2TextBox1.Margin = new System.Windows.Forms.Padding(15, 17, 15, 17);
+			this.guna2TextBox1.Multiline = true;
+			this.guna2TextBox1.Name = "guna2TextBox1";
+			this.guna2TextBox1.PasswordChar = '\0';
+			this.guna2TextBox1.PlaceholderForeColor = System.Drawing.Color.Transparent;
+			this.guna2TextBox1.PlaceholderText = "Random 10 flashcard";
+			this.guna2TextBox1.SelectedText = "";
+			this.guna2TextBox1.SelectionStart = 5;
+			this.guna2TextBox1.ShadowDecoration.Parent = this.guna2TextBox1;
+			this.guna2TextBox1.Size = new System.Drawing.Size(1000, 206);
+			this.guna2TextBox1.TabIndex = 3;
+			// 
+			// btnAllCard
+			// 
+			this.btnAllCard.Animated = true;
+			this.btnAllCard.AutoRoundedCorners = true;
+			this.btnAllCard.BackColor = System.Drawing.Color.Transparent;
+			this.btnAllCard.BorderRadius = 202;
+			this.btnAllCard.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton;
+			this.btnAllCard.CheckedState.Parent = this.btnAllCard;
+			this.btnAllCard.CustomImages.Parent = this.btnAllCard;
+			this.btnAllCard.Font = new System.Drawing.Font("Segoe UI", 9F);
+			this.btnAllCard.ForeColor = System.Drawing.Color.White;
+			this.btnAllCard.HoverState.Parent = this.btnAllCard;
+			this.btnAllCard.Image = ((System.Drawing.Image)(resources.GetObject("btnAllCard.Image")));
+			this.btnAllCard.ImageSize = new System.Drawing.Size(500, 420);
+			this.btnAllCard.Location = new System.Drawing.Point(646, 245);
+			this.btnAllCard.Name = "btnAllCard";
+			this.btnAllCard.ShadowDecoration.Parent = this.btnAllCard;
+			this.btnAllCard.Size = new System.Drawing.Size(474, 406);
+			this.btnAllCard.TabIndex = 1;
+			// 
+			// guna2TextBox2
+			// 
+			this.guna2TextBox2.Animated = true;
+			this.guna2TextBox2.BackColor = System.Drawing.Color.Transparent;
+			this.guna2TextBox2.BorderColor = System.Drawing.Color.Transparent;
+			this.guna2TextBox2.BorderThickness = 0;
+			this.guna2TextBox2.Cursor = System.Windows.Forms.Cursors.IBeam;
+			this.guna2TextBox2.DefaultText = "Sâsas";
+			this.guna2TextBox2.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+			this.guna2TextBox2.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+			this.guna2TextBox2.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+			this.guna2TextBox2.DisabledState.Parent = this.guna2TextBox2;
+			this.guna2TextBox2.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+			this.guna2TextBox2.FillColor = System.Drawing.Color.Transparent;
+			this.guna2TextBox2.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+			this.guna2TextBox2.FocusedState.Parent = this.guna2TextBox2;
+			this.guna2TextBox2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+			this.guna2TextBox2.ForeColor = System.Drawing.Color.Transparent;
+			this.guna2TextBox2.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+			this.guna2TextBox2.HoverState.Parent = this.guna2TextBox2;
+			this.guna2TextBox2.Location = new System.Drawing.Point(855, 1178);
+			this.guna2TextBox2.Margin = new System.Windows.Forms.Padding(15, 17, 15, 17);
+			this.guna2TextBox2.Multiline = true;
+			this.guna2TextBox2.Name = "guna2TextBox2";
+			this.guna2TextBox2.PasswordChar = '\0';
+			this.guna2TextBox2.PlaceholderForeColor = System.Drawing.Color.Transparent;
+			this.guna2TextBox2.PlaceholderText = "Random 10 flashcard";
+			this.guna2TextBox2.SelectedText = "";
+			this.guna2TextBox2.SelectionStart = 5;
+			this.guna2TextBox2.ShadowDecoration.Parent = this.guna2TextBox2;
+			this.guna2TextBox2.Size = new System.Drawing.Size(1000, 206);
+			this.guna2TextBox2.TabIndex = 3;
+			// 
+			// FRevision
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.ClientSize = new System.Drawing.Size(1200, 675);
+			this.Controls.Add(this.guna2TextBox2);
+			this.Controls.Add(this.guna2TextBox1);
+			this.Controls.Add(this.btnBack);
+			this.Controls.Add(this.btnAllCard);
+			this.Controls.Add(this.btnRandom10);
+			this.Controls.Add(this.CtrlMini);
+			this.Controls.Add(this.CtrlClose);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "FRevision";
+			this.Text = "FRevision";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private Guna.UI2.WinForms.Guna2BorderlessForm FRevisions;
+		private Guna.UI2.WinForms.Guna2TextBox guna2TextBox2;
+		private Guna.UI2.WinForms.Guna2TextBox guna2TextBox1;
+		private Guna.UI2.WinForms.Guna2Button btnBack;
+		private Guna.UI2.WinForms.Guna2Button btnAllCard;
+		private Guna.UI2.WinForms.Guna2Button btnRandom10;
+		private Guna.UI2.WinForms.Guna2ControlBox CtrlMini;
+		private Guna.UI2.WinForms.Guna2ControlBox CtrlClose;
+	}
+}
