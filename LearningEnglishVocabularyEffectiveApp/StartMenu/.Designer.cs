5951b7ecb2307dee2385139f5c7ab50d7aa9a77e
=UIT.19522030 <=19522030@gm.uit.edu.vn>
2020-10-14 11:30:46 +0700

    Tạo giao diện Practice

---
+++ LearningEnglishVocabularyEffectiveApp/StartMenu/.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace StartMenu
+{
+    partial class Practice
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Practice));
+            this.guna2ElipseFPractice = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+            this.guna2ImageButton2 = new Guna.UI2.WinForms.Guna2ImageButton();
+            this.gamereturn = new Guna.UI2.WinForms.Guna2ImageButton();
+            this.guna2ImageButton1 = new Guna.UI2.WinForms.Guna2ImageButton();
+            this.BtnGame1 = new Guna.UI2.WinForms.Guna2ImageButton();
+            this.guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // guna2ElipseFPractice
+            // 
+            this.guna2ElipseFPractice.BorderRadius = 10;
+            this.guna2ElipseFPractice.TargetControl = this;
+            // 
+            // guna2ImageButton2
+            // 
+            this.guna2ImageButton2.CheckedState.ImageSize = new System.Drawing.Size(64, 64);
+            this.guna2ImageButton2.CheckedState.Parent = this.guna2ImageButton2;
+            this.guna2ImageButton2.HoverState.ImageSize = new System.Drawing.Size(64, 64);
+            this.guna2ImageButton2.HoverState.Parent = this.guna2ImageButton2;
+            this.guna2ImageButton2.Image = global::StartMenu.Properties.Resources.gameuserPNG;
+            this.guna2ImageButton2.ImageRotate = 0F;
+            this.guna2ImageButton2.Location = new System.Drawing.Point(878, 0);
+            this.guna2ImageButton2.Name = "guna2ImageButton2";
+            this.guna2ImageButton2.PressedState.ImageSize = new System.Drawing.Size(64, 64);
+            this.guna2ImageButton2.PressedState.Parent = this.guna2ImageButton2;
+            this.guna2ImageButton2.Size = new System.Drawing.Size(63, 61);
+            this.guna2ImageButton2.TabIndex = 4;
+            // 
+            // gamereturn
+            // 
+            this.gamereturn.CheckedState.ImageSize = new System.Drawing.Size(64, 64);
+            this.gamereturn.CheckedState.Parent = this.gamereturn;
+            this.gamereturn.HoverState.ImageSize = new System.Drawing.Size(64, 64);
+            this.gamereturn.HoverState.Parent = this.gamereturn;
+            this.gamereturn.Image = global::StartMenu.Properties.Resources.game1return;
+            this.gamereturn.ImageRotate = 0F;
+            this.gamereturn.ImageSize = new System.Drawing.Size(120, 80);
+            this.gamereturn.Location = new System.Drawing.Point(-4, 12);
+            this.gamereturn.Name = "gamereturn";
+            this.gamereturn.PressedState.ImageSize = new System.Drawing.Size(64, 64);
+            this.gamereturn.PressedState.Parent = this.gamereturn;
+            this.gamereturn.Size = new System.Drawing.Size(120, 75);
+            this.gamereturn.TabIndex = 3;
+            // 
+            // guna2ImageButton1
+            // 
+            this.guna2ImageButton1.CheckedState.ImageSize = new System.Drawing.Size(64, 64);
+            this.guna2ImageButton1.CheckedState.Parent = this.guna2ImageButton1;
+            this.guna2ImageButton1.HoverState.ImageSize = new System.Drawing.Size(64, 64);
+            this.guna2ImageButton1.HoverState.Parent = this.guna2ImageButton1;
+            this.guna2ImageButton1.Image = global::StartMenu.Properties.Resources.Capture2;
+            this.guna2ImageButton1.ImageRotate = 0F;
+            this.guna2ImageButton1.ImageSize = new System.Drawing.Size(802, 80);
+            this.guna2ImageButton1.Location = new System.Drawing.Point(211, 445);
+            this.guna2ImageButton1.Name = "guna2ImageButton1";
+            this.guna2ImageButton1.PressedState.ImageSize = new System.Drawing.Size(64, 64);
+            this.guna2ImageButton1.PressedState.Parent = this.guna2ImageButton1;
+            this.guna2ImageButton1.Size = new System.Drawing.Size(791, 80);
+            this.guna2ImageButton1.TabIndex = 2;
+            // 
+            // BtnGame1
+            // 
+            this.BtnGame1.CheckedState.ImageSize = new System.Drawing.Size(64, 64);
+            this.BtnGame1.CheckedState.Parent = this.BtnGame1;
+            this.BtnGame1.HoverState.ImageSize = new System.Drawing.Size(64, 64);
+            this.BtnGame1.HoverState.Parent = this.BtnGame1;
+            this.BtnGame1.Image = ((System.Drawing.Image)(resources.GetObject("BtnGame1.Image")));
+            this.BtnGame1.ImageRotate = 0F;
+            this.BtnGame1.ImageSize = new System.Drawing.Size(798, 82);
+            this.BtnGame1.Location = new System.Drawing.Point(211, 323);
+            this.BtnGame1.Name = "BtnGame1";
+            this.BtnGame1.PressedState.ImageSize = new System.Drawing.Size(64, 64);
+            this.BtnGame1.PressedState.Parent = this.BtnGame1;
+            this.BtnGame1.Size = new System.Drawing.Size(791, 79);
+            this.BtnGame1.TabIndex = 1;
+            // 
+            // guna2PictureBox1
+            // 
+            this.guna2PictureBox1.Image = global::StartMenu.Properties.Resources.Practice;
+            this.guna2PictureBox1.Location = new System.Drawing.Point(-4, 0);
+            this.guna2PictureBox1.Name = "guna2PictureBox1";
+            this.guna2PictureBox1.ShadowDecoration.Parent = this.guna2PictureBox1;
+            this.guna2PictureBox1.Size = new System.Drawing.Size(1211, 684);
+            this.guna2PictureBox1.TabIndex = 0;
+            this.guna2PictureBox1.TabStop = false;
+            // 
+            // Practice
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1198, 676);
+            this.Controls.Add(this.guna2ImageButton2);
+            this.Controls.Add(this.gamereturn);
+            this.Controls.Add(this.guna2ImageButton1);
+            this.Controls.Add(this.BtnGame1);
+            this.Controls.Add(this.guna2PictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Practice";
+            this.Text = "Practice";
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Elipse guna2ElipseFPractice;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
+        private Guna.UI2.WinForms.Guna2ImageButton guna2ImageButton1;
+        private Guna.UI2.WinForms.Guna2ImageButton BtnGame1;
+        private Guna.UI2.WinForms.Guna2ImageButton gamereturn;
+        private Guna.UI2.WinForms.Guna2ImageButton guna2ImageButton2;
+    }
+}
