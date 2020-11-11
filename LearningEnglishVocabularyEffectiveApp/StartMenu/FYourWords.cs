eb562a253afc0f0c74b10e912f13a7c46ed0701e
UIT19522006 <67238266+UIT19522006@users.noreply.github.com>
2020-11-11 18:24:32 +0700

    Cập nhật từ điển anh việt  tra từ đơn giản, mỗi lần 10 từ, chưa kiểm tra từ không tồn tại

--- LearningEnglishVocabularyEffectiveApp/StartMenu/FYourWords.cs
+++ LearningEnglishVocabularyEffectiveApp/StartMenu/FYourWords.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dictionary;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,8 +16,16 @@ namespace StartMenu
 		public FYourWords()
 		{
 			InitializeComponent();
+            string h = "NguyenCongPhi";
+            MessageBox.Show(h);
+            hihi(h);
+            MessageBox.Show(h);
 		}
-
+        public string hihi(string h)
+        {
+            h = h.Substring(3, h.Length - 3);
+            return h;
+        }   
         private void ScrlWord_Scroll(object sender, ScrollEventArgs e)
         {
 
@@ -31,6 +40,17 @@ namespace StartMenu
         private void btnAddWord_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void btFind_Click(object sender, EventArgs e)
+        {
+            if (tbxWord.Text == "") return;
+            WebActivity wa = new WebActivity();
+            string[] Meanings = wa.onlineDictionary(tbxWord.Text,10);
+            for(int i=0;i<Meanings.Length;i++)
+            {
+                this.rtbMean.Text += Meanings[i] + "\n";
+            }    
         }
     }
 }
---
+++ LearningEnglishVocabularyEffectiveApp/StartMenu/WebActivity.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Windows.Forms;
+using System.IO;
+using System.CodeDom.Compiler;
+using System.Runtime.CompilerServices;
+
+namespace Dictionary
+{
+	class WebActivity
+	{
+		public WebActivity()
+		{
+		}
+		public string renderLinkWord(string word)
+		{
+			return "https://iapi.glosbe.com/en/vi/" + word;
+		}
+		public string getSource(string link)
+		{
+			WebClient client = new WebClient();
+			client.Encoding = Encoding.UTF8;
+			client.Headers.Add(HttpRequestHeader.UserAgent, "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0");
+			return client.DownloadString(link);
+		}
+		public void getAudio(string linkaudio, ProgressBar pgb)
+		{
+			using (WebClient wc = new WebClient())
+			{
+				wc.DownloadProgressChanged += wc_DownloadProgressChanged;
+				wc.DownloadFileAsync(new Uri(linkaudio), "temp.mp3");
+			}
+			// Event to track the progress
+			void wc_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+			{
+				pgb.Value = e.ProgressPercentage;
+			}
+		}
+		public string getLinkAudio(string source)
+		{
+			int k = source.IndexOf("data-url-mp3");
+			string h = source;
+			h = h.Substring(k + 14, h.Length - k - 14);
+			string shortlink = "https://iapi.glosbe.com/" + h.Substring(0, h.IndexOf('\"'));
+			return shortlink;
+		}
+		public string[] getMeanings(string source, int n)
+		{
+			string[] ans = new string[10];
+			for(int i=0;i<n;i++)
+			{
+				int k = source.IndexOf("strong class=\" phr\">");
+				source = source.Substring(k + 20, source.Length - k - 20);
+				string res = source.Substring(0, source.IndexOf('<'));
+				ans[i] = res;
+			}	
+			return ans;
+		}
+		public string[] onlineDictionary(string word, int n)
+		{
+			string link = renderLinkWord(word);
+			string source = getSource(link);
+			string[] ans = new string[n];
+			ans = getMeanings(source, n);
+			return ans;
+		}
+		
+	}
+}
