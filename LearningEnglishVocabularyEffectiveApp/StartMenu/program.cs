dbd9cfaeb8e7eeff5a93826b5b98adb5be8916be
UIT19522006 <67238266+UIT19522006@users.noreply.github.com>
2020-11-23 16:59:32 +0700

    Mở lại form ban đầu là form đăng nhập

--- LearningEnglishVocabularyEffectiveApp/StartMenu/program.cs
+++ LearningEnglishVocabularyEffectiveApp/StartMenu/program.cs
@@ -16,7 +16,7 @@ namespace StartMenu
 		{
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new FYourWords());
+			Application.Run(new FLogin());
 		}
 	}
 }
