210993073895e21ff97a36bfbdfe83b016bc1612
=UIT.19522030 <=19522030@gm.uit.edu.vn>
2020-11-27 00:02:22 +0700

    okichua

--- LearningEnglishVocabularyEffectiveApp/StartMenu/GRearangeTheWords.Designer.cs
+++ LearningEnglishVocabularyEffectiveApp/StartMenu/GRearangeTheWords.Designer.cs
@@ -405,8 +405,7 @@
             // 
             // GRearangeTheWords
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(1184, 626);
             this.Controls.Add(this.exit);
