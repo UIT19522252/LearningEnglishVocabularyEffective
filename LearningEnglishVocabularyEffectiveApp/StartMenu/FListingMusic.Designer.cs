f703eb079cad319ac90f8261a239fb82b1074d3e
=UIT.19522030 <=19522030@gm.uit.edu.vn>
2020-12-24 01:14:02 +0700

    music

--- LearningEnglishVocabularyEffectiveApp/StartMenu/FListingMusic.Designer.cs
+++ LearningEnglishVocabularyEffectiveApp/StartMenu/FListingMusic.Designer.cs
@@ -28,36 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.lbNameSong = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label1
+            // treeView
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label1.Location = new System.Drawing.Point(368, 255);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(446, 46);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "ĐÂY LÀ FORM MUSIC";
+            this.treeView.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.treeView.Location = new System.Drawing.Point(0, 3);
+            this.treeView.Name = "treeView";
+            this.treeView.Size = new System.Drawing.Size(316, 532);
+            this.treeView.TabIndex = 1;
+            this.treeView.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Location = new System.Drawing.Point(12, 12);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(828, 467);
+            this.webBrowser.TabIndex = 2;
+            // 
+            // lbNameSong
+            // 
+            this.lbNameSong.Font = new System.Drawing.Font("MV Boli", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNameSong.Location = new System.Drawing.Point(12, 506);
+            this.lbNameSong.Name = "lbNameSong";
+            this.lbNameSong.Size = new System.Drawing.Size(828, 38);
+            this.lbNameSong.TabIndex = 3;
+            this.lbNameSong.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.treeView);
+            this.panel1.Location = new System.Drawing.Point(846, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(324, 535);
+            this.panel1.TabIndex = 4;
             // 
             // FListingMusic
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1182, 556);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lbNameSong);
+            this.Controls.Add(this.webBrowser);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FListingMusic";
             this.Text = "FListingMusic";
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TreeView treeView;
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.Label lbNameSong;
+        private System.Windows.Forms.Panel panel1;
     }
 }
--- LearningEnglishVocabularyEffectiveApp/StartMenu/program.cs
+++ LearningEnglishVocabularyEffectiveApp/StartMenu/program.cs
@@ -16,7 +16,7 @@ namespace StartMenu
 		{
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new FInterface());
+			Application.Run(new FListing());
 		}
 	}
 }
