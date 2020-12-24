ab7d3155261c2dd86caff68ab0ec27add2ae061f
=UIT.19522030 <=19522030@gm.uit.edu.vn>
2020-12-24 15:31:09 +0700

    music oki

--- LearningEnglishVocabularyEffectiveApp/StartMenu/FListingMusic.Designer.cs
+++ LearningEnglishVocabularyEffectiveApp/StartMenu/FListingMusic.Designer.cs
@@ -28,61 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FListingMusic));
             this.treeView = new System.Windows.Forms.TreeView();
-            this.webBrowser = new System.Windows.Forms.WebBrowser();
             this.lbNameSong = new System.Windows.Forms.Label();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel1.SuspendLayout();
+            this.pnList = new System.Windows.Forms.Panel();
+            this.pnMedia = new System.Windows.Forms.Panel();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.pnList.SuspendLayout();
+            this.pnMedia.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
             this.SuspendLayout();
             // 
             // treeView
             // 
             this.treeView.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.treeView.Location = new System.Drawing.Point(0, 3);
+            this.treeView.Location = new System.Drawing.Point(8, 0);
             this.treeView.Name = "treeView";
             this.treeView.Size = new System.Drawing.Size(316, 532);
             this.treeView.TabIndex = 1;
             this.treeView.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
             // 
-            // webBrowser
-            // 
-            this.webBrowser.Location = new System.Drawing.Point(12, 12);
-            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
-            this.webBrowser.Name = "webBrowser";
-            this.webBrowser.Size = new System.Drawing.Size(828, 467);
-            this.webBrowser.TabIndex = 2;
-            this.webBrowser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser_DocumentCompleted);
-            // 
             // lbNameSong
             // 
             this.lbNameSong.Font = new System.Drawing.Font("MV Boli", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbNameSong.Location = new System.Drawing.Point(12, 506);
+            this.lbNameSong.Location = new System.Drawing.Point(12, 437);
             this.lbNameSong.Name = "lbNameSong";
             this.lbNameSong.Size = new System.Drawing.Size(828, 38);
             this.lbNameSong.TabIndex = 3;
+            this.lbNameSong.Text = "Chose the Song from list";
             this.lbNameSong.Click += new System.EventHandler(this.label1_Click_1);
             // 
-            // panel1
+            // pnList
             // 
-            this.panel1.Controls.Add(this.treeView);
-            this.panel1.Location = new System.Drawing.Point(846, 12);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(324, 535);
-            this.panel1.TabIndex = 4;
+            this.pnList.Controls.Add(this.treeView);
+            this.pnList.Location = new System.Drawing.Point(846, 12);
+            this.pnList.Name = "pnList";
+            this.pnList.Size = new System.Drawing.Size(324, 535);
+            this.pnList.TabIndex = 4;
+            // 
+            // pnMedia
+            // 
+            this.pnMedia.Controls.Add(this.axWindowsMediaPlayer1);
+            this.pnMedia.Location = new System.Drawing.Point(19, 15);
+            this.pnMedia.Name = "pnMedia";
+            this.pnMedia.Size = new System.Drawing.Size(821, 402);
+            this.pnMedia.TabIndex = 5;
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(0, -3);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(821, 401);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            this.axWindowsMediaPlayer1.Enter += new System.EventHandler(this.axWindowsMediaPlayer1_Enter);
             // 
             // FListingMusic
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1182, 556);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pnList);
             this.Controls.Add(this.lbNameSong);
-            this.Controls.Add(this.webBrowser);
+            this.Controls.Add(this.pnMedia);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FListingMusic";
             this.Text = "FListingMusic";
-            this.panel1.ResumeLayout(false);
+            this.pnList.ResumeLayout(false);
+            this.pnMedia.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -90,8 +106,9 @@
         #endregion
 
         private System.Windows.Forms.TreeView treeView;
-        private System.Windows.Forms.WebBrowser webBrowser;
         private System.Windows.Forms.Label lbNameSong;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel pnList;
+        private System.Windows.Forms.Panel pnMedia;
+        public AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
     }
 }
--- LearningEnglishVocabularyEffectiveApp/StartMenu/FListingMusic.cs
+++ LearningEnglishVocabularyEffectiveApp/StartMenu/FListingMusic.cs
@@ -19,9 +19,12 @@ namespace StartMenu
         List<string> links= new List<string>();
         string songImagekey = "Song";
         ImageList treeImages = new ImageList();
+        
         public FListingMusic()
         {
             InitializeComponent();
+            
+
             treeImages.Images.Add(songImagekey, Image.FromFile("./images/song.png"));
             SQLconnect();
             InitTreeView();
@@ -47,17 +50,33 @@ namespace StartMenu
                     Song.Nodes.Add(songs[i] + "-" + singers[i], songs[i] + "-" + singers[i], songImagekey, songImagekey);
                 
             }
+            
+            
             this.treeView.AfterSelect += (s, e) =>
             {
                 string fulllink = e.Node.Text;
                 this.lbNameSong.Text = fulllink;
                 string name = fulllink.Substring(0, fulllink.IndexOf("-"));
-                for(int i = 0; i < songs.Count; i++)
+                
+                for (int i = 0; i < songs.Count; i++)
                 {
                     if (name == songs[i])
-                        _ytUrl = links[i];
+                    {
+                        string path = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase);
+                        path = path.Substring(6) + @"\musiclyric\s" + (i+1).ToString() +@".mp3";
+                        this.axWindowsMediaPlayer1.Ctlcontrols.stop();
+
+                        this.axWindowsMediaPlayer1.close();
+                        
+                        this.axWindowsMediaPlayer1.URL = path;
+
+                        //this.axWindowsMediaPlayer1.settings.setMode("loop", true);
+
+                        //this.axWindowsMediaPlayer1.Ctlcontrols.next();
+                        this.axWindowsMediaPlayer1.Ctlcontrols.play();
+                        break;
+                    }
                 }
-                webBrowser.Navigate($"http://youtube.com/v/{VideoId}?version=3");
             };
         }
         private void SQLconnect()
@@ -100,6 +119,11 @@ namespace StartMenu
         }
 
         private void webBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
         {
 
         }
