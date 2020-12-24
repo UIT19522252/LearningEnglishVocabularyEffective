880c20539c3d086a1b7b622f97356ad02e7a0b5d
=UIT.19522030 <=19522030@gm.uit.edu.vn>
2020-12-24 18:03:28 +0700

    clip

--- LearningEnglishVocabularyEffectiveApp/StartMenu/FListingMusic.Designer.cs
+++ LearningEnglishVocabularyEffectiveApp/StartMenu/FListingMusic.Designer.cs
@@ -42,9 +42,9 @@
             // treeView
             // 
             this.treeView.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.treeView.Location = new System.Drawing.Point(8, 0);
+            this.treeView.Location = new System.Drawing.Point(0, 0);
             this.treeView.Name = "treeView";
-            this.treeView.Size = new System.Drawing.Size(316, 532);
+            this.treeView.Size = new System.Drawing.Size(324, 532);
             this.treeView.TabIndex = 1;
             this.treeView.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
             // 
